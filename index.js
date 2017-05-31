fd58de2c88f3debd0c011309a68cddf4e8b70c5e
juanantona <juan.antonadelval@gmail.com>
2017-05-31 23:57:03 +0200

    Login first approach and used ES6 syntax

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
-const app = express()
+const app     = express()
 
+// Set template engine
 const dust = require('consolidate').dust
 app.engine('dust', dust)
 app.set('view engine', 'dust')
 
-app.get('/', function(req, res) {
-	res.render('index', { title : 'This is the way!' })
+// Routes
+app.get('/', (req, res) => {
+	res.redirect('/login')
 })
 
-app.listen(3000, function(){
+app.get('/login', (req, res) => {
+	res.render('index', {title:'This is the way!'})
+})
+
+// Server up
+app.listen(3000, () => {
 	console.log('App up on port 3000!')
 })
