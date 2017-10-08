b47b8d26da1aa9bd5e03c7b48d02f08f61d1e85a
Juanan <juan.antonadelval@gmail.com>
2017-10-09 00:37:42 +0200

    JS client refactor

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,80 +1,104 @@
+let dom = document;
+
+let getByClass = (className, parentElement) => {
+	let elements = (parentElement || dom).getElementsByClassName(className)
+	return (!elements.length || elements.length ==1)? elements[0] : elements
+}
+
+let getById = (className) => {
+	return dom.getElementById(className)
+}
+
+let hasClass = (element, className) => {
+	if (element.className.indexOf(className) > 0) return true
+	return false 	
+}
+
+let removeClass = (element, className) => {
+	let classes = element.className.split(' ')
+	    classes = classes.filter( (element_class) => { return element_class !== className })
+	element.className = classes.join(' ')
+}
+
+let addClass = (element, className) => {
+	if (!hasClass(element, className)) element.className += ' ' + className
+}
+
+let changeClass = (element, className) => {
+	if (hasClass(element, className)) removeClass(element, className)
+	else addClass(element, className)	
+}
+
+let createGroups = (members, people_each_group) => {
+	let groups = [], random_index
+
+	while (members.length > 0) {
+    var group = [];
+    while (group.length < people_each_group && members.length > 0) {
+    	random_index = Math.floor( members.length*Math.random() )
+    	group = group.concat( members.splice(random_index - 1, 1) )
+    }
+    groups.push(group)
+	}
+	return groups
+}
+
 window.onload = () => {
   
-	let randomy_button    = document.getElementsByClassName('js-randomy')[0];
+	let randomy_button = getByClass('js-randomy')
 	
 	if (randomy_button) {
 		randomy_button.addEventListener('click', function(ev) {
-			let people_each_group = document.getElementById('peopleEachGroup').value;
-		  let members         = Array.from( document.getElementsByClassName('list-container')[0].getElementsByClassName('js-member') )
-	 	  let groups_textarea = document.getElementsByClassName('js-groups-result')[0]
+			let people_each_group = getById('peopleEachGroup').value
+			let members_list      = getByClass('list-container')
+		  let members           = Array.from( getByClass('js-member', members_list) )
+	 	  let groups_textarea   = getByClass('js-groups-result')
 	 	  
-	 	  var groups = createGroups(members, parseInt(people_each_group));
-
-	 	  let gruop_list = document.getElementsByClassName('js-gruop-list')[0];
+	 	  let groups     = createGroups(members, parseInt(people_each_group))
+	 	  let gruop_list = getByClass('js-gruop-list')
 	 	  while (gruop_list.firstChild) { gruop_list.removeChild(gruop_list.firstChild) }
 
-	 	  var list_item_template = '<li>GROUP: '+ + '</li>'
-
-	 	  groups.forEach(function(group, index) {
+	 	  groups.forEach( (group, index) => {
 				var list_items = ''
 				var text = ''
 				group.forEach((member, idx) => {
 		 	  	list_items += member.value
 		 	  	if (idx !== group.length - 1) list_items += ', '
 		 	  })
-		 	  var li = document.createElement('li');
+		 	  var li = dom.createElement('li');
 		 	  var text = 'GROUP '+ (index + 1) + ': ' + list_items
-	 	  	li.appendChild(document.createTextNode(text))
+	 	  	li.appendChild(dom.createTextNode(text))
 	 	  	gruop_list.appendChild(li);
 	 	  })
-	 	});
+	 	})
 	}
 		
-	let show_menu_button = document.getElementsByClassName('js-show-menu')[0];
+	let show_menu_button    = getByClass('js-show-menu')
+	let nav_element         = getByClass('nav-menu')
+	let show_members_button = getByClass('js-show-members')
+	let hide_members_button = getByClass('js-hide-members')
+  let members_layer       = getByClass('mobile-members-list')
+	
+	if (show_menu_button) {
+		show_menu_button.addEventListener('click', (ev) => {
+			ev.preventDefault();
+			changeClass(nav_element, '_menu_open')
+		})
+	}	
 
-	show_menu_button.addEventListener('click', function(ev) {
-		ev.preventDefault();
-		let nav_element = document.getElementsByClassName('nav-menu')[0];
-		if (nav_element.className.indexOf('_menu_open') > 0) nav_element.className = 'nav-menu';
-		else nav_element.className += ' _menu_open';
-	})
+	if (show_members_button) {
+		show_members_button.addEventListener('click', (ev) => {
+			ev.preventDefault();
+			addClass(members_layer, '_open')
+			addClass(hide_members_button, '_open')
+		})
+	}	
 
-	let show_members = document.getElementsByClassName('js-show-members')[0];
-	let hide_members = document.getElementsByClassName('js-hide-members')[0];
-
-	show_members.addEventListener('click', function(ev) {
-		ev.preventDefault();
-		let members_layer = document.getElementsByClassName('mobile-members-list')[0];
-		if (members_layer.className.indexOf('_open') > 0) members_layer.className = 'container mobile-members-list';
-		else members_layer.className += ' _open';
-		if (hide_members.className.indexOf('_open') > 0) hide_members.className = 'mobile-members-close js-hide-members';
-		else hide_members.className += ' _open';
-	})
-
-
-
-	hide_members.addEventListener('click', function(ev) {
-		ev.preventDefault();
-		let members_layer = document.getElementsByClassName('mobile-members-list')[0];
-		if (members_layer.className.indexOf('_open') > 0) members_layer.className = 'container mobile-members-list';
-		else members_layer.className += ' _open';
-		if (hide_members.className.indexOf('_open') > 0) hide_members.className = 'mobile-members-close js-hide-members';
-		else hide_members.className += ' _open';
-	})
-
-};
-
-function createGroups(members, people_each_group) {
-	var groups = [];
-	var random_index;
-
-	while (members.length > 0) {
-    var group = [];
-    while (group.length < people_each_group && members.length > 0) {
-    	random_index = Math.floor(members.length*Math.random());
-    	group = group.concat(members.splice(random_index - 1, 1));
-    }
-    groups.push(group);
+	if (hide_members_button) {
+		hide_members_button.addEventListener('click', (ev) => {
+			ev.preventDefault();
+			removeClass(members_layer, '_open')
+			removeClass(hide_members_button, '_open')
+		})
 	}
-	return groups;
 }
